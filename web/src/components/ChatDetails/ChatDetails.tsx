bcc62ae13d2e0b81e471a464dfedd381de766447
Animesh <animesh.kumar@thoughtspot.com>
2023-05-13 18:14:15 +0530

    basic settings

--- web/src/components/ChatDetails/ChatDetails.tsx
+++ web/src/components/ChatDetails/ChatDetails.tsx
@@ -1,32 +1,34 @@
 import React from "react";
 import { Chat } from "../Chat/Chat";
 
-
 const mockMessages = [
-    {
-      id: "1",
-      text: "Hello There",
-      sender: "Caller",
-    },
-    {
-      id: "2",
-      text: "Hi There",
-      sender: "Me",
-    },
-    {
-      id: "1",
-      text: "Hello, this is longer text Hello, this is longer text Hello, this is longer text Hello, this is longer text",
-      sender: "Caller",
-    },
-    {
-      id: "2",
-      text: "Hello, this is longer text Hello, this is longer text Hello, this is longer text Hello, this is longer text",
-      sender: "Me",
-    },
-  ];
+  {
+    id: "1",
+    text: "Hello There",
+    sender: "Caller",
+  },
+  {
+    id: "2",
+    text: "Hi There",
+    sender: "Me",
+  },
+  {
+    id: "1",
+    text: "Hello, this is longer text Hello, this is longer text Hello, this is longer text Hello, this is longer text",
+    sender: "Caller",
+  },
+  {
+    id: "2",
+    text: "Hello, this is longer text Hello, this is longer text Hello, this is longer text Hello, this is longer text",
+    sender: "Me",
+  },
+  {
+    id: "2",
+    text: "Summary: This was a sales call aimed at selling insurance, The insurer is HDFC and the caller was offering a term plan. The caller was polite and well spoken.",
+    sender: "Me",
+  },
+];
 
 export const ChatDetails = () => {
-  return (
-    <Chat disableChat messagesList={mockMessages} />
-  );
+  return <Chat disableChat messagesList={mockMessages} />;
 };
--- web/src/components/Settings/Settings.tsx
+++ web/src/components/Settings/Settings.tsx
@@ -1,6 +1,34 @@
 import React from "react";
-import './Settings.scss';
+import "./Settings.scss";
+import { Checkbox, Spacer, Text } from "@nextui-org/react";
 
 export const Settings = () => {
-  return <div></div>;
+  return (
+    <div className="settings-container">
+      <br />
+      <Text h3 className="settings-title">
+        Settings
+      </Text>
+      <div className="settings-list">
+        <Checkbox
+          isRounded
+          defaultSelected
+          color="primary"
+          className="setting-item"
+          size="sm"
+        >
+          Auto-answer Spam Calls
+        </Checkbox>
+        <Checkbox
+          isRounded
+          defaultSelected
+          color="primary"
+          className="setting-item"
+          size="sm"
+        >
+          Show notifications
+        </Checkbox>
+      </div>
+    </div>
+  );
 };
