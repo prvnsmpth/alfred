76611dbab1be077c64cb00769ba2a40e1cd8da28
Praveen Sampath <praveen.sampath@thoughtspot.com>
2023-05-13 14:40:50 +0530

    Add websocket server

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, send_from_directory, send_file
+from flask_socketio import SocketIO, send, emit
 
 app = Flask(__name__, static_url_path='', static_folder='web/static')
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
@@ -9,3 +11,10 @@ def index():
 @app.route("/hello")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@socketio.on('connect')
+def test_connect(auth):
+    emit('alfred_msg', {'data': 'Connected'})
+
+if __name__ == '__main__':
+    socketio.run(app)
